79fd6ae75b8b25acef4e6e5fb8f46c5baea527ac
aditya-hcl <119669900+aditya-hcl@users.noreply.github.com>
2023-04-24 16:06:35 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Sriparno!";
+        return "Hello Aditya!";
     }
 
 }
